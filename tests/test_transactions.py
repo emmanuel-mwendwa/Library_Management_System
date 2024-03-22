25d120464d9723f4e9f6e0b814dcd771ebe93876
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-03-22 09:16:05 +0300

    Test view transactions

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -83,3 +83,21 @@ class TransactionTestCase(BaseTestConfig):
 
             updated_book = Book.query.get(book.id)
             self.assertEqual(updated_book.available_copies, 6)
+
+
+    def test_view_transactions(self):
+
+        user = self.create_user()
+
+        with self.client:
+
+            headers = {
+                "Authorization": f"Basic {base64.b64encode(f'{user.email}:password123'.encode('utf-8')).decode('utf-8')}",
+                "Content-Type": "application/json"
+            }
+
+            response = self.client.get('/api/v1/view_transactions', headers=headers)
+
+            self.assertEqual(response.status_code, 200)
+            data = response.json
+            self.assertIn('transactions', data)
